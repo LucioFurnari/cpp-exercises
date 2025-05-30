ce74d9d01607e3ed892c69a4e2912158bba481ee
LucioFurnari <luciofurnari99@gmail.com>
2025-05-30 16:12:52 -0300

    feat: complete exercise

--- Practice exercises_3/08/main.cpp
+++ Practice exercises_3/08/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 //  a) Implementar la función invertir_cadena(cadena) que muestre una cadena pasada
@@ -7,7 +8,18 @@ using namespace std;
 //    llame a la función creada en el punto anterior para informar el resultado en
 //    pantalla.
 
+string reverse_string(string text) {
+  string result {};
+  for (int i=text.length()-1; i >= 0; i--) {
+    result += text[i];
+  }
+  return result;
+}
+
 int main() {
-  
+  string text {};
+  cout << "Enter a text to reverse it: ";
+  cin >> text;
+  cout << "The reverse of the text is: " << reverse_string(text) << endl;
   return 0;
 }
