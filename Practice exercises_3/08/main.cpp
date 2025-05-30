506f6e4d72467d19dd64c81d9d7be94bae968142
LucioFurnari <luciofurnari99@gmail.com>
2025-05-30 15:16:23 -0300

    feat: add description

--- Practice exercises_3/08/main.cpp
+++ Practice exercises_3/08/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+//  a) Implementar la función invertir_cadena(cadena) que muestre una cadena pasada
+//    por parámetro en orden inverso.
+//  b) Escribir un programa que solicite el ingreso por teclado de una cadena y luego
+//    llame a la función creada en el punto anterior para informar el resultado en
+//    pantalla.
+
 int main() {
   
   return 0;
