15029c39741c6137aae63d46c779b90ceb948315
LucioFurnari <luciofurnari99@gmail.com>
2025-06-01 17:26:24 -0300

    feat: add menu and functions

--- Practice exercises_3/14/main.cpp
+++ Practice exercises_3/14/main.cpp
@@ -14,9 +14,36 @@ using namespace std;
 //  ● Asegúrate de manejar adecuadamente los casos donde no hay fondos suficientes
 //  para realizar un retiro.
 
-int balance = { 0 }; 
+float balance = { 0 };
+
+void add_amount(float amount) {
+  balance += amount;
+}
+
+void remove_amount(float amount) {
+  // Check if have money.
+  if (balance == 0 || balance - amount < 0) {
+    cout << "You have no money." << endl;
+  }
+  balance -= amount;
+}
+
+void show_menu() {
+  cout << "A: Enter an amount into the account." << endl;
+  cout << "B: Withdraw an amount from the account." << endl;
+  cout << "C: Display the current account balance." << endl;
+  cout << "D: Exit the program." << endl;
+}
 
 int main() {
+  char option {};
+
+  cout << "Welcome, what operation do you want to do: " << endl;
+
+  do {
+    show_menu();
+    cin >> option;
+  } while(toupper(option) != 'D');
 
   return 0;
 }
