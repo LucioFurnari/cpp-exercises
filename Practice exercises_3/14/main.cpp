8ffc97d0397798558975b909c8238faf187c5e5c
LucioFurnari <luciofurnari99@gmail.com>
2025-06-01 03:50:08 -0300

    feat: start exercise 14

---
+++ Practice exercises_3/14/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Escribe un programa en C++ que simule la gestión de una cuenta bancaria. El
+// programa debe permitir al usuario realizar las siguientes operaciones:
+//  ● Ingresar un monto a la cuenta.
+//  ● Retirar un monto de la cuenta.
+//  ● Mostrarel saldo actual de la cuenta.
+//  Requisitos:
+//  ● Utiliza una variable global para almacenar el saldo total de la cuenta.
+//  ● Implementa funciones con variables locales para realizar cada una de las
+//  operaciones mencionadas.
+//  ● Asegúrate de manejar adecuadamente los casos donde no hay fondos suficientes
+//  para realizar un retiro.
+
+int balance = { 0 }; 
+
+int main() {
+
+  return 0;
+}
