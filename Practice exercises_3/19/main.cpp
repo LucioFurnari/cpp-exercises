c2336b3859aa4280944839066eac519a6da48592
LucioFurnari <luciofurnari99@gmail.com>
2025-06-06 12:03:07 -0300

    feat: add exercise 19

---
+++ Practice exercises_3/19/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Ordenamiento de números:
+//  a) Desarrolla una función que ordena en forma ascendente los elementos de un
+//  arreglo de 10 números enteros.
+//  Por ejemplo, el arreglo con los elementos [2,5,1,4,4,8,12,9,7,11] quedaría
+//  ordenado como [1,2,4,4,5,7,8,9,11,12]
+//  b) ¿Qué debería modificar del algoritmo anterior para que los ordene en orden
+//  descendente?
+
+void sort_ascending(int list[10]) {
+
+}
+
+int main() {
+  int list[10] = {2,5,1,4,4,8,12,9,7,11};
+
+
+
+  return 0;
+}
