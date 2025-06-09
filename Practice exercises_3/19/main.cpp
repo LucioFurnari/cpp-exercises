4e0cbb337cb3ca2cd6d308c0de4246ed201e251b
LucioFurnari <luciofurnari99@gmail.com>
2025-06-09 17:39:43 -0300

    feat: change code

--- Practice exercises_3/19/main.cpp
+++ Practice exercises_3/19/main.cpp
@@ -9,14 +9,30 @@ using namespace std;
 //  b) ¿Qué debería modificar del algoritmo anterior para que los ordene en orden
 //  descendente?
 
-void sort_ascending(int list[10]) {
+// void sort_ascending(int list[10]) {
 
-}
+// }
 
 int main() {
   int list[10] = {2,5,1,4,4,8,12,9,7,11};
 
+  for(int i=0; i < 10; i++) {
+    int minNumber = i;
 
+    for(int j=i+1; j < 10; j++) {
+      if(list[j] < list[minNumber]) {
+        minNumber = j;
+      }
+    }
+
+    if(minNumber != i) {
+      int temp = list[minNumber];
+      list[minNumber] = list[i];
+      list[i] = temp;
+    }
+  }
 
   return 0;
 }
+// 2,5,1,4,4,8,12,9,7,11 i=0
+// 1,5,2,4,4,8,12,9,7,11 i=1
