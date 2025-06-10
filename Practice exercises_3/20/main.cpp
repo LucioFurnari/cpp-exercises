c09da1dc82177d365bef26b7eddbf7575b8e1b10
LucioFurnari <luciofurnari99@gmail.com>
2025-06-10 16:26:54 -0300

    feat: fix for loop

--- Practice exercises_3/20/main.cpp
+++ Practice exercises_3/20/main.cpp
@@ -11,19 +11,21 @@ using namespace std;
 
 int main() {
   unordered_map<int, int> listMap;
-  const int length = 6;
+  int length = 6;
   int list[] = {2,5,2,5,5,1};
 
-  for(int n=0; n < length; n++) {
-    cout << list[n] << " ";
-  }
   for(int i=0; i < length; i++) {
     if(listMap.count(list[i]) > 0) {
-      for(int j=i; i < length-1; j++)
+      for(int j=i; j < length-1; j++)
         list[j] = list[j+1];
+      length--;
+      i--;
     } else {
       listMap[list[i]] = 1;
     }
+  }
+  for(int n=0; n < length; n++) {
+    cout << list[n] << " ";
   }
   cout << "Number of duplicated 2: " << listMap[2] << endl;
 
