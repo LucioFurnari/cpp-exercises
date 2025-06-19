9b74c18627da1ed8ff4af683beaf5a484bd858e0
LucioFurnari <luciofurnari99@gmail.com>
2025-06-18 23:43:16 -0300

    feat: add add.cpp file

---
+++ Headers_declaration/add.cpp
@@ -0,0 +1,6 @@
+#include "add.h"         // copies forward declaration from add.h here
+
+int add(int x, int y)
+{
+    return x + y;
+}
--- Headers_declaration/add.h
+++ Headers_declaration/add.h
@@ -1,7 +1,3 @@
 // We really should have a header guard here, but will omit it for simplicity (we'll cover header guards in the next lesson)
 
-// definition for add() in header file -- don't do this!
-int add(int x, int y)
-{
-    return x + y;
-}
+int add(int x, int y);
