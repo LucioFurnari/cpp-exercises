0a21d30634b3eb43197936e7518d2b7088926977
LucioFurnari <luciofurnari99@gmail.com>
2025-06-26 21:28:58 -0300

    feat: add folder with exercise with multiple files

---
+++ External_file_ex/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "add.h"
+using namespace std;
+
+int main() {
+
+  cout << "THe sum of 3 and 4 is: " << add(3, 4) << '\n';
+   std::cin.get(); // Keeps the window open
+  return 0;
+}
