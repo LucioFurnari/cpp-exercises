45b11f702182cdf7dd0ff85c9e5aae59982f71e7
LucioFurnari <luciofurnari99@gmail.com>
2025-09-12 23:27:25 -0300

    feat: changes

--- Programacion_imperativa/15/main.cpp
+++ Programacion_imperativa/15/main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 // Escribe un programa que busque un número en una matriz dada y devuelva su posición. Si el número no se 
 // encuentra, debe indicarlo.
 
-int find_number(int matrix[][3], int rows, int columns, int value) {
+int find_number(int matrix[][3], int rows, int columns, int value, int & rowPos, int & columnPos) {
 
   for(int r=0; r<rows; r++) {
     for(int c=0; c<columns; c++) {
       if(matrix[r][c] == value) {
-        
+        rowPos = r;
+        columnPos = c;
       }
     }
   }
@@ -21,13 +22,19 @@ int find_number(int matrix[][3], int rows, int columns, int value) {
 int main() {
   const int rows = 3;
   const int columns = 3;
+  int rowPos = -1;
+  int columnPos = -1;
   int matrix[rows][columns] {
     {1,2,3},
     {4,5,6},
     {7,8,9},
   };
-
-
+  find_number(matrix, rows, columns, 5, rowPos, columnPos);
+  if(rowPos > 0) {
+    cout << "The number is in the row: " << rowPos << " and column: " << columnPos << endl;
+  } else {
+    cout << "The number could not be found" << endl;
+  }
 
   return 0;
 }
