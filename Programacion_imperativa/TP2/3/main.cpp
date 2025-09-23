7478635254d804090dda06e0e2a94606978aa0cf
LucioFurnari <luciofurnari99@gmail.com>
2025-09-22 23:50:17 -0300

    feat: add new exercise

---
+++ Programacion_imperativa/TP2/3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Escribir un programa en C++ que realice una búsqueda binaria en un arreglo de números enteros donde algunos 
+// números pueden llegar a repetirse. El programa debe encontrar la primera aparición del número buscado e 
+// informarlo en pantalla.
+
+
+int main() {
+  
+  return 0;
+}
