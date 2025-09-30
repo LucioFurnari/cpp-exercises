68559dc958a89f7fb283556b4dd90004990525b4
LucioFurnari <luciofurnari99@gmail.com>
2025-09-30 20:38:08 -0300

    feat: add new exercise

--- Programacion_imperativa/TP2/3/main.cpp
+++ Programacion_imperativa/TP2/3/main.cpp
@@ -6,7 +6,31 @@ using namespace std;
 // informarlo en pantalla.
 
 
+int binary_search(int arr[], int value, int dl) {
+  int start = 0;
+  int end = dl-1;
+
+  while(start <= end) {
+    int mid = (start+end) / 2;
+    if(arr[mid] == value) {
+      return mid;
+    } else {
+      if(arr[mid] > value) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+  }
+
+  return -1;
+}
+
 int main() {
-  
+  int dl = 10;
+  int arr[10] = {2,2,1,3,5,6,8,8,9,4};
+
+  int result = binary_search(arr, 8, dl);
+  cout << "Result: " << result;
   return 0;
 }
