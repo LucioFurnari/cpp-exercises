3ee39c05f161eb9b96205d4782feb55f6f7afc7c
LucioFurnari <luciofurnari99@gmail.com>
2025-10-15 23:40:51 -0300

    feat: add new exercise

---
+++ Programacion_imperativa/TP2/5/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Escribir un programa en C++ que realice las siguientes tareas:
+// a) generar, aleatoriamente, una lista (arreglo) con 100 números enteros en el rango de 0 a 100.000;
+// b) Ordenar el listado en modo creciente por el método de la burbuja;
+// c) Ordenar el listado en modo decreciente por el método de la burbuja. 
+
+int main() {
+
+  return 0;
+}
