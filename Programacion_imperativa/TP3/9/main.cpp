9c79e7fb2998a93ed6e0689c4d5ca301959bf5b2
LucioFurnari <luciofurnari99@gmail.com>
2025-11-07 17:13:48 -0300

    feat: add new code

---
+++ Programacion_imperativa/TP3/9/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+/* Desarrollar un programa que realice las siguientes operaciones en listas enlazadas simples de números enteros.
+  A. Cargar una lista insertando los elementos ordenados en forma descendente. Finaliza luego de ingresar
+  el cero, que también se debe agregar a la lista.
+  B. Eliminar las ocurrencias de los números que son múltiplos de 3.
+  C. Imprimir la lista antes y después de la eliminación */
+
+
+struct Node
+{
+  int value;
+  Node* next;
+};
+
+
+void organizeList(Node* & list, Node * newNode) {
+
+  if(list == nullptr || newNode->value > list->value) {
+    newNode->next = list;
+    list = newNode;
+  } else {
+    Node* aux = list;
+
+    while (aux->next != nullptr && aux->next->value > newNode->value) 
+    {
+      aux = aux->next;
+    }
+    newNode->next = aux->next;
+    aux->next = newNode;
+  }
+}
+
+void showList(Node* & list) {
+  Node* aux = list;
+  while (aux != nullptr)
+  {
+    cout << "Number: " << aux->value << endl;
+    aux = aux->next;
+  }
+  
+}
+
+void createList(Node* & list) {
+  int number;
+
+  while (true)
+  {
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if(number == 0) {
+      Node* newNode = new Node{number, nullptr};
+      organizeList(list, newNode);
+      break;
+    } else {
+      Node* newNode = new Node{number, nullptr};
+      organizeList(list, newNode);
+    }
+  }
+  
+}
+
+int main() {
+  Node* list = nullptr;
+
+  createList(list);
+  showList(list);
+  return 0;
+}
