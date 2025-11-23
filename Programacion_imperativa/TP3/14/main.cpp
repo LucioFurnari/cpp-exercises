f283f4bb66336f26891f8c051dfad91e114c6958
LucioFurnari <luciofurnari99@gmail.com>
2025-11-23 20:17:12 -0300

    feat: add new exercise

---
+++ Programacion_imperativa/TP3/14/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+  Generar una lista con los datos personales de los alumnos de un colegio (legajo, apellido y nombre, DNI) y las
+  notas de tres materias: matemática, literatura y geografía (para cada alumno se cargarán las tres materias). Las
+  notas pueden ir de 1 a 10 y se debe validar que el usuario no ingrese notas incorrectas, volviendo a pedir una
+  nueva nota cada vez que ingrese una incorrecta, hasta que ingrese un valor en el rango indicado. Se pide
+  informar la cantidad de alumnos que aprobaron las tres materias (se aprueba con 7) y el porcentaje que
+  representan los aprobados sobre el total de alumnos.
+*/
+
+struct Student {
+  string legajo;
+  string lastName;
+  string name;
+  int DNI;
+  int math;
+  int literature;
+  int geography;
+}
+
+int main() {1
+
+
+  return 0;
+}
