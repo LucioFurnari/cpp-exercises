a4f0cc6a61871671a2908c99e1c9fd04b764d8a6
LucioFurnari <luciofurnari99@gmail.com>
2025-11-25 20:41:34 -0300

    feat: add functions

--- Programacion_imperativa/TP3/14/main.cpp
+++ Programacion_imperativa/TP3/14/main.cpp
@@ -16,13 +16,81 @@ struct Student {
   string lastName;
   string name;
   int DNI;
-  int math;
-  int literature;
-  int geography;
+  int mathGrade;
+  int literatureGrade;
+  int geographyGrade;
+};
+
+struct Node
+{
+  Student student;
+  Node* next;
+};
+
+void enterGrade(int & subject) {
+  int grade;
+
+  cout << "Enter the grade of the subject (greater than 1 and less or equal to 10)";
+  cin >> grade;
+  while (grade < 1 || grade > 10)
+  {
+    cout << "Enter the grade of the subject (greater than 1 and less or equal to 10)";
+    cin >> grade;
+  }
+  
+  subject = grade;
 }
 
-int main() {1
+Student createStudent() {
+  Student student;
 
+  cout << "Enter the legajo: ";
+  getline(cin, student.legajo);
+  cout << "Enter the last name of the student: ";
+  getline(cin, student.lastName);
+  cout << "Enter the name of the student: ";
+  getline(cin, student.name);
+  enterGrade(student.mathGrade);
+  enterGrade(student.literatureGrade);
+  enterGrade(student.geographyGrade);
 
+  return student;
+}
+
+void createList(Node* & list) {
+  if(list == nullptr) {
+    list->student = createStudent();
+    list->next = nullptr;
+  } else {
+    Node* aux = list;
+    for (int i = 0; i < 4; i++)
+    {
+      while (aux->next != nullptr)
+      {
+        aux = aux->next;
+      }
+      Student student = createStudent();
+      aux->next = new Node{student, nullptr};
+    }
+    
+  }
+}
+
+void showList(Node* & list) {
+  Node* tmp = list;
+
+  while (tmp != nullptr)
+  {
+    cout << tmp->student.name << endl;
+    tmp = tmp->next;
+  }
+  
+}
+
+int main() {
+  Node* head = nullptr;
+
+  createList(head);
+  showList(head);
   return 0;
 }
